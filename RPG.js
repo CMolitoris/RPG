309dccb3f6c05a61c74d45eb03b4327244515f97
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-08-27 16:27:03 -0400

    Attack specifications added and 'attack' method in progress

--- RPG.js
+++ RPG.js
@@ -3,21 +3,30 @@
 class character {
     constructor(name,health,attPower,classType) {
         this.name = name;
-        this.health = health;
-        this.attPower = attPower;
+        this.stunned = false;
         this.dead = false;
         if(classType==="HERO") {
+            this.health = 200;
+            this.attPower = 40;
             this.attackSet = ["Helm-Splitter","Siesmic Toss","Earthshatter"];
         } else if(classType ==="ALLY") {
+            this.health = 100;
+            this.attPower = 15;
             this.attackSet = ["Slash","Tackle"];
         } else {
+            this.health = 100;
+            this.attPower = 20;
             this.attackSet = ["Maul","Thrash"];
         }
     }
 
     helmSplitter(character) {
         console.log("A devastating blow!");
-        let dmg = character.attPower + 30;
+        let dmg = character.attPower + 20;
+        let chance = randNum(100);
+        if(chance<10) {
+            dmg *= 1.5;
+        }
         return dmg;
     }
     seisToss(character) {
@@ -42,12 +51,20 @@ class character {
     }
     maul(character) {
         console.log("An onslaught!");
-        let dmg = character.attPower + 20;
+        let dmg = character.attPower + 12;
+        let chance = randNum(100);
+        let counter;
+        while(chance<20) {
+            counter++;
+            dmg += 12;
+            chance = randNum(100);
+        }
+        console.log("The enemy attacked " + counter + " times!");
         return dmg;
     }
     thrash(character) {
         console.log("The enemy attacks with furry!");
-        let dmg = character.attPower + 15;
+        let dmg = character.attPower + 25;
         return dmg;
     }
 
@@ -78,21 +95,22 @@ let turnCounter;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function randNum(max) {
     let min = 0;
     let max = max;
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function attack(character) {
+    for(let i=0;i<character.attackSet.lenght;i++) {
+        console.log((i+1) + ": " + character.attackSet[i] + "\n");
+    }
+    let choice = prompt("Which attack would you like to use?");  //"Helm-Splitter","Siesmic Toss","Earthshatter"
+    if(character.classType==="HERO") {
+        switch(choice) {
+            case "1":
+            character.helmSplitter
+        }
+    }
+    
 }
